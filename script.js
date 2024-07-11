5d436a6cb2772add5a88a3e5e85389a56506dc07
AyushiBadika <badikaayushi@gmail.com>
2024-07-11 16:26:27 +0530

    completed assignment

--- script.js
+++ script.js
@@ -92,10 +92,14 @@ function createDivs() {
   symbolContainer.forEach((singleSymbol, index) => {
     singleSymbol.addEventListener("click", () => {
       console.log("clicked");
+      noOfAttempts.innerHTML = Number(noOfAttempts.innerHTML) + 1;
+      symbol[index].classList.add("activeSymbol");
+
+      symbolContainer[index].classList.add("activeSymbolContainer");
+
       setTimeout(() => {
-        noOfAttempts.innerHTML = Number(noOfAttempts.innerHTML) + 1;
         gameFunctionality(index);
-      }, 200);
+      }, 1000);
     });
   });
 
@@ -107,28 +111,26 @@ function createDivs() {
     console.log("matching pairs" + matchingPairIndex);
     console.log("matching pairs" + matchingPairs);
 
-    symbolContainer[index].classList.add("activeSymbolContainer");
-    symbol[index].classList.add("activeSymbol");
-
     if (matchPairTimeOut) {
       clearTimeout(matchPairTimeOut);
     }
 
     matchPairTimeOut = setTimeout(() => {
       matchPairs();
-    }, 500);
+    }, 800);
   }
 
   function matchPairs() {
     while (matchingPairs.length > 1) {
-      console.log(matchingPairs.length);
       if (matchingPairs[0] !== matchingPairs[1]) {
         symbolContainer[matchingPairIndex[0]].classList.remove(
           "activeSymbolContainer"
         );
+
         symbolContainer[matchingPairIndex[1]].classList.remove(
           "activeSymbolContainer"
         );
+
         symbol[matchingPairIndex[0]].classList.remove("activeSymbol");
         symbol[matchingPairIndex[1]].classList.remove("activeSymbol");
       }
